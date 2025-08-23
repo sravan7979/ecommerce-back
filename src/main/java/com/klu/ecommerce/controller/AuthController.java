9c783462f14acb25f0d6cedaa7fe499c84f7044d
sravan7979 <sravankumar@MATURUs-MacBook-Air.local>
2025-08-23 10:06:33 +0530

    Backend cors

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:8080/ecommerce")
 public class AuthController {
     private final UserService userService;
 
